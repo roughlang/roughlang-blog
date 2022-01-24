57241586e5c0fbdb0fbd0af2e58bc97b5c29ef71
roughlang <roughlangx@gmail.com>
2022-01-24 17:43:03 +0900

    top RESR API for wordpress

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -18,6 +18,31 @@
       <h2>Blog</h2>
 
       <script type="text/javascript" src="/assets/js/vue/vue.js"></script>
+      <script type="text/javascript" src="/assets/js/vue/axios.js"></script>
+      
+      <script>
+        /* Blogの表示 */
+        const blog = new Vue({
+          el: '#blog',
+          data: {
+            blog: []
+          },
+          mounted: function() {
+            const self = this;
+            /* posts */
+            axios.get( 'https://lwbase.roughlang.com/ac/wp-json/wp/v2/posts?page=1')
+            .then(function(response) {
+              // console.log(response.data);
+              self.blog = response.data;
+            }).catch(function(){
+              console.log('Failed to get blog from wp-rest-api.', error);
+            });
+          }
+        });
+      </script>
+      <div id="blog">
+
+      </div>
       https://idee.roughlang.com/ac/wp-json/wp/v2/posts
       {{ env('APP_NAME') }}
       <ul>
