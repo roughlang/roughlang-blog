7c6236389709482ac707422d9f5c427e5219c0c0
aonuma <aonuma.mori@gmail.com>
2022-03-11 20:23:59 +0900

    よくわからない

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -12,53 +12,27 @@
 @endsection
 
 @section('content_block')
-<div class="container lw-contents-block mt100">
-  <div class="row mb100">
+<div class="container lw-contents-block mt100 mb100">
+  <div class="row">
     <div class="col-sm-6">
+      <h2>Blog</h2>
 
-      <h2>Blog update</h2>
-      <div id="blog" class="blog-top">
-        <div v-for="(row,index) in blog" v-bind:key="row.id" class="blog-ul">
-          <div class="blog-li">
-            <a v-bind:href="row.link">@{{row.title.rendered}}</a>
-            <span class="blog-date">@{{row.modified_gmt}}</span><!--(@{{index}})-->
-          </div>
-        </div>
-      </div>
-
-      <h2 class="mt50">Blog index</h2>
-      <div id="blog" class="blog-top mb40">
-        <div class="blog-ul">
-          <div class="blog-li">
-            <a href="https://idee.roughlang.com/ac/menu_of_philosophy/">哲学の目次</a>
-          </div>
-        </div>
-      </div>
-
+      <script type="text/javascript" src="/assets/js/vue/vue.js"></script>
+      <script type="text/javascript" src="/assets/js/vue/axios.js"></script>
+      
       <script>
         /* Blogの表示 */
         const blog = new Vue({
           el: '#blog',
           data: {
-            loading: false,
             blog: []
           },
           mounted: function() {
             const self = this;
             /* posts */
-            this.loading = true;
-            axios.get( 'https://idee.roughlang.com/ac/wp-json/wp/v2/posts?page=1')
+            axios.get( 'https://lwbase.roughlang.com/ac/wp-json/wp/v2/posts?page=1')
             .then(function(response) {
-              for( key in response.data ) {
-                /* 日付のフォーマット変更 */
-                var date = response.data[key].modified_gmt.split('T');
-
-                var date_format = date[0].replace('-', '.');
-                var date_format = date[0].replace(/-/g, '.');
-                console.log(date_format);
-                response.data[key].modified_gmt = date_format;
-              }
-              this.loading = false;
+              // console.log(response.data);
               self.blog = response.data;
             }).catch(function(){
               console.log('Failed to get blog from wp-rest-api.', error);
@@ -66,33 +40,19 @@
           }
         });
       </script>
-    </div>
+      <div id="blog">
 
-    <div class="col-sm-6 mb30">
-      <div id="blog" class="blog-top">
-        <div class="imgwrap">
-          <a href="#"><img src="/assets/img/top/bl_001.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_002.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_003.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_004.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_005.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_006.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_007.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_008.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_009.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_010.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_011.jpg" alt=""></a>
-          <a href="#"><img src="/assets/img/top/bl_012.jpg" alt=""></a>
-        </div>
       </div>
-  </div>
-</div>
-
-
-
-<!--
+      https://idee.roughlang.com/ac/wp-json/wp/v2/posts
+      {{ env('APP_NAME') }}
+      <ul>
+        <li><a href="https://github.com/roughlang/lwbase" target="_blank">https://github.com/roughlang/lwbase</a></li>
+        <li><a href="http://localhost:8000/ac/?feed=rss2" target="_blank">http://localhost:8000/ac/?feed=rss2</a></li>
+        <li><a href="https://lwbase.roughlang.com/ac/wp-json/wp/v2/posts" target="_blank">https://lwbase.roughlang.com/ac/wp-json/wp/v2/posts</a></li>
+      </ul>  
+    </div>
     <div class="col-sm-6">
-      <h2>Blog update</h2>
+      <h1>Hello!!</h1>
       <div id="app">@{{ message }}</div>
       
       <script>
@@ -106,7 +66,7 @@
 
       <div id="list">
         <ul v-for="(row,index) in tests" :key="row.title">
-            <li>@{{row.title}}(@{{index}})- @{{row.content}}　@{{row.content}}</li>
+            <li>@{{row.title}}(@{{index}})- @{{row.content}}</li>
             <ul v-for="(image,index) in row.images">
               <li>@{{image}}</li>
             </ul>
@@ -125,7 +85,27 @@
         })
       </script>
     </div>
-      -->
+  </div>
+</div>
+
+<div class="container lw-contents-block mb60">
+  <div class="row">
+    <div class="col-sm-6">
+      <p>ある朝、グレゴール・ザムザが気がかりな夢から目ざめたとき、自分がベッドの上で一匹の巨大な毒虫に変ってしまっているのに気づいた。彼は甲殻のように固い背中を下にして横たわり、頭を少し上げると、何本もの弓形のすじにわかれてこんもりと盛り上がっている自分の茶色の腹が見えた。腹の盛り上がりの上には、かけぶとんがすっかりずり落ちそうになって、まだやっともちこたえていた。ふだんの大きさに比べると情けないくらいかぼそいたくさんの足が自分の眼の前にしょんぼりと光っていた。</p>
+      <p style="font-weight: 100;">ある朝、グレゴール・ザムザが気がかりな夢から目ざめたとき、自分がベッドの上で一匹の巨大な毒虫に変ってしまっているのに気づいた。彼は甲殻のように固い背中を下にして横たわり、頭を少し上げると、何本もの弓形のすじにわかれてこんもりと盛り上がっている自分の茶色の腹が見えた。腹の盛り上がりの上には、かけぶとんがすっかりずり落ちそうになって、まだやっともちこたえていた。ふだんの大きさに比べると情けないくらいかぼそいたくさんの足が自分の眼の前にしょんぼりと光っていた。</p>
+      <p style="font-weight: 200;">ある朝、グレゴール・ザムザが気がかりな夢から目ざめたとき、自分がベッドの上で一匹の巨大な毒虫に変ってしまっているのに気づいた。彼は甲殻のように固い背中を下にして横たわり、頭を少し上げると、何本もの弓形のすじにわかれてこんもりと盛り上がっている自分の茶色の腹が見えた。腹の盛り上がりの上には、かけぶとんがすっかりずり落ちそうになって、まだやっともちこたえていた。ふだんの大きさに比べると情けないくらいかぼそいたくさんの足が自分の眼の前にしょんぼりと光っていた。</p>
+      <p style="font-weight: 300;">ある朝、グレゴール・ザムザが気がかりな夢から目ざめたとき、自分がベッドの上で一匹の巨大な毒虫に変ってしまっているのに気づいた。彼は甲殻のように固い背中を下にして横たわり、頭を少し上げると、何本もの弓形のすじにわかれてこんもりと盛り上がっている自分の茶色の腹が見えた。腹の盛り上がりの上には、かけぶとんがすっかりずり落ちそうになって、まだやっともちこたえていた。ふだんの大きさに比べると情けないくらいかぼそいたくさんの足が自分の眼の前にしょんぼりと光っていた。</p>
+      <p style="font-weight: 400;">ある朝、グレゴール・ザムザが気がかりな夢から目ざめたとき、自分がベッドの上で一匹の巨大な毒虫に変ってしまっているのに気づいた。彼は甲殻のように固い背中を下にして横たわり、頭を少し上げると、何本もの弓形のすじにわかれてこんもりと盛り上がっている自分の茶色の腹が見えた。腹の盛り上がりの上には、かけぶとんがすっかりずり落ちそうになって、まだやっともちこたえていた。ふだんの大きさに比べると情けないくらいかぼそいたくさんの足が自分の眼の前にしょんぼりと光っていた。</p>
+    </div>
+    <div class="col-sm-6">
+      <p>Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheuren Ungeziefer verwandelt. Er lag auf sinem panzerartig harten Rücken und sah, wenn er den Kopf eine wenig hob, seinen gewölbten, braunen, von bogenförmigen Versteifungen geteilten Bauch, auf dessen Höhe sich die Bettdecke, zum gänzlichen Niedergleiten bereit, kaum noch erhalten konnte. Seine vielen, im Vergleich zu seinem sonstigen Umfang kläglich dünnnen Beine flimmerten ihm hilflos vor den Augen.</p>
+      <p style="font-weight: 100;">Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheuren Ungeziefer verwandelt. Er lag auf sinem panzerartig harten Rücken und sah, wenn er den Kopf eine wenig hob, seinen gewölbten, braunen, von bogenförmigen Versteifungen geteilten Bauch, auf dessen Höhe sich die Bettdecke, zum gänzlichen Niedergleiten bereit, kaum noch erhalten konnte. Seine vielen, im Vergleich zu seinem sonstigen Umfang kläglich dünnnen Beine flimmerten ihm hilflos vor den Augen.</p>
+      <p style="font-weight: 200;">Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheuren Ungeziefer verwandelt. Er lag auf sinem panzerartig harten Rücken und sah, wenn er den Kopf eine wenig hob, seinen gewölbten, braunen, von bogenförmigen Versteifungen geteilten Bauch, auf dessen Höhe sich die Bettdecke, zum gänzlichen Niedergleiten bereit, kaum noch erhalten konnte. Seine vielen, im Vergleich zu seinem sonstigen Umfang kläglich dünnnen Beine flimmerten ihm hilflos vor den Augen.</p>
+      <p style="font-weight: 300;">Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheuren Ungeziefer verwandelt. Er lag auf sinem panzerartig harten Rücken und sah, wenn er den Kopf eine wenig hob, seinen gewölbten, braunen, von bogenförmigen Versteifungen geteilten Bauch, auf dessen Höhe sich die Bettdecke, zum gänzlichen Niedergleiten bereit, kaum noch erhalten konnte. Seine vielen, im Vergleich zu seinem sonstigen Umfang kläglich dünnnen Beine flimmerten ihm hilflos vor den Augen.</p>
+      <p style="font-weight: 400;">Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheuren Ungeziefer verwandelt. Er lag auf sinem panzerartig harten Rücken und sah, wenn er den Kopf eine wenig hob, seinen gewölbten, braunen, von bogenförmigen Versteifungen geteilten Bauch, auf dessen Höhe sich die Bettdecke, zum gänzlichen Niedergleiten bereit, kaum noch erhalten konnte. Seine vielen, im Vergleich zu seinem sonstigen Umfang kläglich dünnnen Beine flimmerten ihm hilflos vor den Augen.</p>
+    </div>
+  </div>
+</div>
 @endsection
 
 
