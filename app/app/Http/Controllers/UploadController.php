64797b7973332c1e50a123976652fb27e07f8c49
roughlang <roughlangx@gmail.com>
2022-03-28 02:10:50 +0900

    uploader(3)

--- app/app/Http/Controllers/UploadController.php
+++ app/app/Http/Controllers/UploadController.php
@@ -31,6 +31,12 @@ class UploadController extends Controller
    */
   public function save(Request $request)
   {
+    $validated = $request->validate([
+      'file' => ['nullable','file','image','mimes:jpg,jpeg,png,bmp,gif,svg,webp','dimensions:min_width=50,min_height=50,max_width=5000,max_height=5000'],
+    ],[
+      'file.image' => 'jpg,jpeg,png,bmp,gif,svg,webp以外の拡張子は利用できません。',
+      'file.dimensions' => '縦横5000px以下のサイズでアップロードしてください。',
+    ]); 
     /**
      * get files
      */
--- app/resources/views/uploader/uploader.blade.php
+++ app/resources/views/uploader/uploader.blade.php
@@ -20,8 +20,6 @@
       <div class="co-uploader" id="co-uploader">
       <!-- https://reffect.co.jp/vue/drag-drop-file-upload -->
         
-      
-
         <div id="uploader">
           <div
             class="drop_area"
@@ -29,8 +27,7 @@
             @dragleave="dragLeave"
             @dragover.prevent 
             @drop.prevent="dropFile" 
-            :class="{enter: isEnter}"
-          > 
+            :class="{enter: isEnter}"> 
               ファイルアップロード
           </div>
 
@@ -38,11 +35,16 @@
 
           <div>
             <ul>
-              <li v-for="file in files">@{{ file.name }} @{{ file.lastModified }}</li>
+              <li v-for="text in message">
+                @{{ text }}
+              </li>
             </ul>
             <ul>
-              <li v-for="upload_file in upload_files">@{{ upload_file.name }} @{{ upload_file.lastModified }}
-              </li>
+              <li v-for="file in upload_files">@{{ file.name }} @{{ file.lastModified }}</li>
+            </ul>
+            <ul>
+              <!-- <li v-for="upload_file in upload_files">@{{ upload_file.name }} @{{ upload_file.lastModified }}
+              </li> -->
             </ul>
           </div>
           <div>
@@ -54,29 +56,48 @@
             </ul>
           </div>
         </div>
-      
 
         <script>
           const uploader = new Vue({
             el: "#uploader",
             data: {
               isEnter: false,
+              file: null,
               files: [],
               upload_files: [],
-              // files_path: [],
-              images: []
+              images: [],
+              message: []
             },
             methods: {
               upload:function(){
-                // alert("hoges");
-                console.log(this.upload_files);
+                // console.log(this.upload_files);
+                this.images = [];
+                this.message = [];
                 this.upload_files.forEach(file => {
                   let form = new FormData()
                   form.append('file', file)
                   axios.post('/save', form).then(response => {
-                    console.log(response.data)
+                    // for (let i = 0; i < response.length; ++i) {
+                    //   consoloe.log(response.data);
+                    // }
+                    // console.log(response.data);
+                    console.log(response.status);
+                    if (response.status == '200') {
+                      this.message = ['アップロードが完了しました。'];
+                      this.files = [];
+                      this.upload_files = [];
+                      this.images = [];
+                    }
+                    console.log(this.message);
                   }).catch(error => {
-                    console.log(error)
+                    this.message = [];
+                    console.log(error.response.status);
+                    console.log(error.response.statusText);
+                    console.log(error);
+                    if (error.response.status == '422') {
+                      this.message = ['送信エラーが発生しました。最初からやり直してください。'];
+                    }
+                    
                   })
                 })
               },
@@ -91,43 +112,63 @@
               //   console.log('DragOver')
               // },
               dropFile() {
-                /* file set */
+                this.files = [];
+                this.images = [];
+                this.message = [];
                 this.files = [...event.dataTransfer.files];
                 /* push array */
                 for (let n = 0; n < this.files.length; ++n) {
-                  this.upload_files.push(this.files[n]);
+                  // console.log(this.files[n].type);
+                  if (
+                    this.files[n].type == 'image/jpeg' ||
+                    this.files[n].type == 'image/png' ||
+                    this.files[n].type == 'image/gif'
+                  ) {
+                    this.upload_files.push(this.files[n]);
+                  } else {
+                    this.message.push("アプロードできないフォーマットです。");
+                  }
                 }
+
                 /* display thumbnail */
-                for (let i = 0; i < this.files.length; ++i) {
+                for (let i = 0; i < this.upload_files.length; ++i) {
                   const reader = new FileReader()
                   reader.onload = () => {
                     this.images.push(reader.result)
                   }
-                  reader.readAsDataURL(this.files[i])
+                  reader.readAsDataURL(this.upload_files[i])
                 }
-                /* ajax */
-                // this.upload_files.forEach(file => {
-                //   let form = new FormData()
-                //   form.append('file', file)
-                //   axios.post('/save', form).then(response => {
-                //     console.log(response.data)
-                //   }).catch(error => {
-                //     console.log(error)
-                //   })
-                // })
 
 
+                
+                // this.files = [...event.dataTransfer.files];
+                /* push array */
+                // for (let n = 0; n < this.files.length; ++n) {
+                //   console.log(this.files[n].type);
+                //   if (this.files[n].type == 'image/jpeg') {
+                //     this.upload_files.push(this.files[n]);
+                //   } else {
+                //     this.message.push("アプロードできないフォーマットです。");
+                //   }
+                // }
+                /* display thumbnail */
+                // for (let i = 0; i < this.upload_files.length; ++i) {
+                //   const reader = new FileReader()
+                //   reader.onload = () => {
+                //     this.images.push(reader.result)
+                //   }
+                //   reader.readAsDataURL(this.upload_files[i])
+                // }
                 this.isEnter = false;
               },
               delete_file:function(index){
                 console.log(index);
+                this.files.splice(index, 1)
                 this.upload_files.splice(index, 1)
                 this.images.splice(index, 1)
               }
             }
-            // console.log(files);
-          })
-          
+          }) 
         </script>
       </div><!-- co-uploader -->
 
