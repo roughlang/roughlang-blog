05171e5154798a4894802c6082f1f8a453ed9f7b
roughlang <roughlangx@gmail.com>
2022-03-30 15:40:24 +0900

    logout追加

--- app/resources/views/include/nav.blade.php
+++ app/resources/views/include/nav.blade.php
@@ -14,7 +14,9 @@
 						<a class="nav-link active" aria-current="page" href="/">Home</a>
 					</li>
 					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
+						<a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Blog
+            </a>
 						<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
 							<li><a class="dropdown-item" href="/ac">Top</a></li>
 							<li><a class="dropdown-item" href="/ac/category">Category</a></li>
@@ -23,25 +25,39 @@
 							<li><a class="dropdown-item" href="/ac/gallery">Gallery</a></li>
 						</ul>
 					</li>
-					@if (Route::has('login'))
-						@auth
-						<li class="nav-item"><a class="nav-link active" href="{{ url('/home') }}">Home</a></li>
-						@else
-						<li class="nav-item"><a class="nav-link active" href="{{ route('login') }}">Log in</a></li>
-							@if (Route::has('register'))
-							<li class="nav-item"><a class="nav-link active" href="{{ route('register') }}">Register</a></li>
-							@endif
-						@endauth
-					@endif
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Menu</a>
-						<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-							<li><a class="dropdown-item" href="/vue">vue.js</a></li>
-							<li><a class="dropdown-item" href="#">Another action</a></li>
-							<li><hr class="dropdown-divider"></li>
-							<li><a class="dropdown-item" href="#">Something else here</a></li>
-						</ul>
-					</li>
+          <li class="nav-item"><a  class="nav-link active" href="#">foo</a></li>
+          @guest
+            @if (Route::has('login'))
+              @auth
+              <li class="nav-item"><a class="nav-link active" href="{{ url('/home') }}">Home</a></li>
+              @else
+              <li class="nav-item"><a class="nav-link active" href="{{ route('login') }}">Log in</a></li>
+                @if (Route::has('register'))
+                <li class="nav-item"><a class="nav-link active" href="{{ route('register') }}">Register</a></li>
+                @endif
+              @endauth
+            @endif
+          @else
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Menu</a>
+              <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                <li><a class="dropdown-item" href="/vue">vue.js</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a  class="nav-link active" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+              </a>
+            </li>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+          @endguest
 				</ul>
 				<form class="d-flex">
 					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -49,6 +65,7 @@
 						<img src="/assets/img/item/search.svg" width="24" height="24" alt="search">
 					</button>
 				</form>
+       
 			</div>
   	</div>
   </nav>
