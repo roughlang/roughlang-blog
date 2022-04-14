a28d3b3f4ef0a3021c41a8761434e35dc0b3b62c
roughlang <roughlangx@gmail.com>
2022-04-14 14:48:38 +0900

    twitter delete

--- app/public/twitter/moriri/data/account-creation-ip.js
+++
@@ -1,8 +0,0 @@
-window.YTD.account_creation_ip.part0 = [
-  {
-    "accountCreationIp" : {
-      "accountId" : "835689007275073537",
-      "userCreationIp" : "60.94.37.1"
-    }
-  }
-]
--- app/public/twitter/moriri/data/account-timezone.js
+++
@@ -1,8 +0,0 @@
-window.YTD.account_timezone.part0 = [
-  {
-    "accountTimezone" : {
-      "accountId" : "835689007275073537",
-      "timeZone" : "Pacific Time (US & Canada)"
-    }
-  }
-]
